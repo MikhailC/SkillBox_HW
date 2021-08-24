0c3186cdc36e5be9fcb8bc5bc432396888a65033
Mikhail Chesnokov <mikhail.chesnokov@gmail.com>
2021-08-24 07:23:22 +0300

    пошли делать следуюшее дз

---
+++ Hw6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Log2(1_000_000_000_000));
+        }
+    }
+}
